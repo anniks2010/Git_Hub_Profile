7d7ada2a4b3c8c04c8d8bb1749f9eba8426fda0c
Opilane TTHK <opilane@tthk.ee>
2020-01-24 11:41:18 +0200

    commit

---
+++ Git_Hub_Profile/ProfileData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_Hub_Profile
+{
+    public class ProfileData
+    {
+        public string Login { get; set; }
+        public int Public_repos { get; set; }
+        public int Followers { get; set; }
+        public int Following { get; set; }
+        public string Avatar_url { get; set; }
+    }
+}
---
+++ Git_Hub_Profile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+
+namespace Git_Hub_Profile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.github.com/users/anniks2010?client_id=6031e0d559160618be74&client_secret=ecac0f6e3c0a0964f53d4022645ba4ea887f3d88";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            request.UserAgent = "Foo";
+            request.Accept = "*/*";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd(); ////panen valmis koha, kuhu maha lugeda, mis on olemas vastuses
+                ProfileData profileData = JsonConvert.DeserializeObject<ProfileData>(response);
+                Console.WriteLine($"Login: {profileData.Login}");
+                Console.WriteLine($"Public repos:{profileData.Public_repos}");
+                Console.WriteLine($"Followers:{profileData.Followers}");
+                Console.WriteLine($"Following:{profileData.Following}");
+                Console.WriteLine($"Avatar url:{profileData.Avatar_url}");
+
+
+            }
+        }
+    }
+}
